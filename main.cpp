4bdeac3ae604064b0d31482754cbafa2b813c303
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-09-29 09:56:25 -0500

    lee cadena y regresa tamaño

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+    string cadena;
+    getline(cin, cadena); //lee hasta \n
+    cout<<cadena.size()<<endl;
     return 0;
 }
